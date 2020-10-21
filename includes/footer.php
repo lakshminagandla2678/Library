61e6e99bc09da52562ead0c8d6286b56e35ba55d
Lakshmi Nagandla <lakshmi.nagandla@Aspiresys.com>
2020-10-21 21:01:11 +0530

    Initial commit

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; AspireSysems India pvt ltd. Library Management System |<a href="https://phpgurukul.com/" target="_blank"  > Designed by : AspireSystems</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
